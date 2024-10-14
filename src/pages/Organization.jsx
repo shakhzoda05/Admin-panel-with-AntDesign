b0c4d3672794439388296b549cb450c3b9dd2854
shakhzoda05 <shakhzoda05@gmail.com>
2024-10-14 05:10:59 +0500

    add font

--- src/pages/Organization.jsx
+++ src/pages/Organization.jsx
@@ -1,9 +1,16 @@
+import { Button } from 'antd'
 import React from 'react'
 
 function Organization() {
     return (
-        <div>
-            <h2>Organization</h2>
+        <div className='p-5'>
+            <div className='flex items-center justify-between'>
+                <div>
+                    <h2 className='font-bold text-[25px]'>Tashkilotlar</h2>
+                    <span className='text-[15px] pl-1 text-slate-400'>tashkilotlar(0)</span>
+                </div>
+                <Button icon size='large' type='primary'>Qo'shish</Button>
+            </div>
         </div>
     )
 }
